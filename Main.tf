ca499bc82a309712a4cf3eef182edcb2dd442d60
RakeshKumar4470 <rk42942@gmail.com>
2024-12-15 15:24:36 +0530

    new code

--- Main.tf
+++ Main.tf
@@ -2,30 +2,33 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.113.0"
+      version = "4.3.0"
     }
   }
 }
+
 provider "azurerm" {
   features {}
 }
-resource "azurerm_resource_group" "jasmine" {
-  name     = "Denver"
+resource "azurerm_resource_group" "rg-tf-lanka" {
+  name     = "fortune"
   location = "centralindia"
 }
 
-resource "azurerm_storage_account" "pride" {
-  name                     = "paracute"
-  resource_group_name      = "Denver"
+resource "azurerm_storage_account" "SA_FT_01" {
+  depends_on               = [azurerm_resource_group.rg-tf-lanka]
+  name                     = "veg-oil"
   location                 = "centralindia"
+  resource_group_name      = "fortune"
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "Denver"
-    storage_account_name = "paracute"
-    container_name       = "bricks"
-    key                  = "proud.terraform.tfstate"
+    backend "azurerm" {
+      resource_group_name  = "fortune"
+      storage_account_name = "veg-oil"
+      container_name       = "proud"
+      key                  = "proud.terraform.tfstate"
+    }
   }
-}
+
